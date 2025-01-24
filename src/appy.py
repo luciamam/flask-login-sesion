d11c2bf3af8cde4774e7a953437b74fa0d8c0d08
luciamam <lumanmba2023@gmail.com>
2025-01-24 12:30:48 +0100

    todas las rutas ya estan protegidas , y  redireccionas como tiene que ser , cuando el usuario ya esta autentificado

--- src/appy.py
+++ src/appy.py
@@ -3,7 +3,7 @@ from config import config
 from flask_mysqldb import MySQL
 from config import config
 #importacion para el login 
-from flask_login import LoginManager,login_user,logout_user,login_required
+from flask_login import LoginManager,login_user,logout_user,login_required,current_user
 
 #Models
 from models.ModelUser   import ModelUser
@@ -51,9 +51,18 @@ def login():
     
     else:
 
+        if current_user.is_authenticated:
+            print("AUTENTIFICADO CON SESION ")
+            print(current_user.username)
+            print(current_user.fullname)
+            print(current_user.get_id())
+
+            return redirect(url_for('home'))
+        
         return render_template('auth/login.html')
 
 @app.route('/home')
+@login_required
 def home():
     return render_template('home.html')
 
@@ -61,6 +70,34 @@ def home():
 def index():
     return redirect(url_for('login'))
 
+
+
+@app.route('/logout')
+def logout():
+
+    logout_user()
+    return redirect(url_for('login'))
+
+
+#ruta protegida
+@app.route('/protected')
+@login_required
+def protected():
+    return "<h1>Esto es una pagina para usuarios auntetificados "
+
+
+
+def status_401(error):
+    return redirect(url_for('login'))
+
+
+
+#para rutas que no existen 
+def status_404(error):
+    return '<h1>Pagina no encontrada </h1>'
+
 if __name__=='__main__':
     app.config.from_object(config['development'])
+    app.register_error_handler(401,status_401)
+    app.register_error_handler(404,status_404)
     app.run()
